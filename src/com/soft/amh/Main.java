2b446fc157e1ba8a8f3561a8428ba529728394a2
AungMyatHtut-Developer <85837899+AungMyatHtut-Developer@users.noreply.github.com>
2025-03-10 20:39:51 +0630

    Drawing Basic Shapes and Demo Game Loop

--- src/com/soft/amh/Main.java
+++ src/com/soft/amh/Main.java
@@ -7,3 +7,21 @@ public class Main {
         new GameWindow();
     }
 }
+
+
+
+/*
+1. Build Project & Create Game Screens
+2. Drawing basic shapes and demo game loop
+3. Create a good game loop
+4. Adding Grids and required screens
+5. Drawing Blocks
+6. Changing Block Shapes
+7. Randomly Generate Blocks
+8. Collision Detection
+9. Stacking Block on each other
+10. Removing full lines of blocks and score
+11. Store high score in file
+12. Adding game sound effects and music
+13. Export application file into production
+ */
--- src/com/soft/amh/constants/GameConstants.java
+++ src/com/soft/amh/constants/GameConstants.java
@@ -2,6 +2,6 @@ package com.soft.amh.constants;
 
 public class GameConstants {
 
-    public static final int GAME_SCREEN_HEIGHT = 500;
-    public static final int GAME_SCREEN_WIDTH = 300;
+    public static final int GAME_SCREEN_HEIGHT = 800;
+    public static final int GAME_SCREEN_WIDTH = 500;
 }
